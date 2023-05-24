4b9e371a752f144915973e18d3c6b91fc9301890
BondVitalii <122458459+BondVitalii@users.noreply.github.com>
2023-05-24 23:00:48 +0300

    hw-gallery-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,64 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const listGalleryRef = document.querySelector('.gallery');
+const galleryItemsRef = createGalleryItems(galleryItems);
+
+listGalleryRef.innerHTML = galleryItemsRef;
+
+let instance;
+
+listGalleryRef.addEventListener('click', onListGalleryClick);
+
+function createGalleryItems(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item" >
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
+
+function onListGalleryClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const originalSrc = event.target.dataset.source;
+
+  instance = basicLightbox.create(
+    `<img src="${originalSrc}" width="800" height="600">`,
+    {
+      onShow: () => {
+        console.log('Добавили ESC');
+        document.addEventListener('keydown', onPressEscape);
+      },
+      onClose: () => {
+        console.log('Убрали ESC');
+        document.removeEventListener('keydown', onPressEscape);
+      },
+    }
+  );
+  instance.show();
+}
+
+function onPressEscape(event) {
+  if (event.key === 'Escape') {
+    instance.close(() => {
+      console.log('Закрыли, когда нажали ESC');
+    });
+  }
+}
+
+// console.log(galleryItems);
